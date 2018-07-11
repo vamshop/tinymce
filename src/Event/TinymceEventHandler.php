88db73107d95826680a1755211ef6ecccf36de1f
vam <vamshop@gmail.com>
2018-07-11 14:56:05 +0300

    Смена названий

--- src/Event/TinymceEventHandler.php
+++ src/Event/TinymceEventHandler.php
@@ -12,7 +12,7 @@ use Croogo\Core\Croogo;
  * @category Event
  * @package  Tinymce
  * @license  http://www.opensource.org/licenses/mit-license.php The MIT License
- * @link     http://www.croogo.org
+ * @link     http://www.vamshop.com
  */
 class TinymceEventHandler implements EventListenerInterface
 {
